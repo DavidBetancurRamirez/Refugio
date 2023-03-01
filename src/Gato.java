1504a82411a6cc21c15b9f5ff845c1d79285c382
SB-0111 <47514458+SB-0111@users.noreply.github.com>
2023-03-01 07:09:29 -0500

    Clases Animal y Chequeo

---
+++ src/Gato.java
@@ -0,0 +1,6 @@
+public class Gato extends Animal{
+
+    public Gato(String raza, String recomendaciones, int edad, double cantidadComida, Alimentacion alimentacion, Enfermedad[] enfermedades) {
+        super(raza, recomendaciones, edad, cantidadComida, alimentacion, enfermedades);
+    }
+}
---
+++ src/Perro.java
@@ -0,0 +1,6 @@
+public class Perro extends Animal{
+
+    public Perro(String raza, String recomendaciones, int edad, double cantidadComida, Alimentacion alimentacion, Enfermedad[] enfermedades) {
+        super(raza, recomendaciones, edad, cantidadComida, alimentacion, enfermedades);
+    }
+}
